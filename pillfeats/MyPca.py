34857a288373ef483e442aa8685fd0acd7dbb25c
Benjamin Joseph <benjo@sayhello.com>
2015-02-21 00:10:44 -0800

    pca on pill paired data

---
+++ pillfeats/MyPca.py
@@ -0,0 +1,68 @@
+#!/usr/bin/python
+
+import numpy as np
+import numpy.linalg
+
+class MyPca():
+    def fit(self, x, ndimsout):
+        #print np.cov(feats.transpose, axis).shape
+        mean =  np.mean(x, axis=0).reshape(1, x.shape[1])
+        meanmat = np.tile(mean, (x.shape[0], 1))
+        #print meanmat.shape
+        nomeanfeats = x - meanmat
+        P = np.cov(nomeanfeats.transpose())
+        d = np.sqrt(np.diagonal(P))
+
+        for j in range(nomeanfeats.shape[1]):
+            nomeanfeats[:, j] = nomeanfeats[:, j] / d[j]
+    
+        P2 = np.cov(nomeanfeats.transpose())
+
+        w, v = np.linalg.eig(P2)
+        #f = np.matrix(v) * np.matrix(np.diag(w)) * np.matrix(v.transpose());
+        #print f - P2
+        # decomposition is E * lambda * E'
+        #  X = [ndata,nstates], X'*X = [nstates,nstates]
+        #
+        #  Ergo (X - xm)'*(X - Xm) = P2 = E * lambda * E'
+        #        E'*(X - xm)'*(X - xm)*E = lambda
+        #  post-multiply by eigenvectors to get transformed
+        #print ((P2 + 1e-6)*10).astype('int')
+        print v
+        print w
+        transform = v[:, 0:ndimsout]
+        
+        self.covdiags_ = d
+        self.explainedvariance_ = d / np.sum(d)
+        self.mean_ = mean
+        self.transform_ = transform
+        #self.reducedfeats_ = f2
+        
+     #takes a bunch of row vectors   
+    def transform(self, x):
+        #subtrac tmean
+        meanmat = np.tile(self.mean_, (x.shape[0], 1))
+        nomeanfeats = x - meanmat
+        
+        #normalize by variance
+        for j in range(nomeanfeats.shape[1]):
+            nomeanfeats[:, j] = nomeanfeats[:, j] / self.covdiags_[j]
+        
+        #"rotate" and reduce dimensions
+        retmat =  np.matrix(nomeanfeats) * np.matrix(self.transform_)
+        return np.array(retmat)
+        
+    def toDict(self):
+        me = {}
+        me['covdiags'] = self.covdiags_.tolist()
+        me['transform'] = self.transform_.tolist()
+        me['mean'] = self.mean_.tolist()
+        return me
+        
+    def setFromDict(self, me):
+        self.covdiags_ = np.array(me['covdiags'])
+        self.transform_ = np.array(me['transform'])
+        self.mean_ = np.array(me['mean'])
+
+
+
---
+++ pillfeats/pca_pill_pairs.py
@@ -0,0 +1,104 @@
+#!/usr/bin/python
+
+import sys
+import datetime
+import requests
+import sys
+import numpy as np
+from pylab import *
+from copy import *
+import time
+import json
+from MyPca import MyPca
+
+k_uri = 'https://api.hello.is/v1/datascience/pill/'
+
+my_users = {'fm' : '3.120ca851b92d49cdae5e0e33eb02f171', 
+            'mi' : '3.ea82af0895024eb2998150e1e9f83365', 
+            'ki' : '4.9a1f7741f22a46959d7923a53f15776e', 
+            'sp' : '4.4a4a066a56164203b0f2828096bcdc01', 
+            'mar' : '3.9ffd3285bdc54adba5bfa74fc09bfdee', 
+            'am' : '3.2260a267c7b54ad093d765c63f066dcc', 
+            'ab' : '3.a64f9383d16948498ef26ac7e46cc636', 
+            'tp' : '3.4893f18df31a42fdbe4672fede166a73', 
+            'mf' : '3.7976dee8ee464a26861f77d942bd0b9f', 
+            'af' : '2.1d8b5016a7384d6784e1787480da5a38'
+            }
+            
+user_partners = [['ki', 'sp'], 
+                ['fm', 'ab'], 
+                ['mar', 'mf'], 
+                ['mi', 'tp'], 
+                ['am', 'af']]
+
+
+def get_info_for_day_from_server(auth, datestring):
+    
+    headers = {'Authorization' : 'Bearer %s' % auth}
+    response = requests.get(k_uri + datestring,params={},headers = headers)
+        
+    if (response.ok):
+        data = response.json()
+        print ('got %d records of pill data' % (len(data)))
+        return data
+    else:
+        return []
+    
+def get_pair_data(pairs, datestr):
+    
+    data1 = get_info_for_day_from_server(my_users[pairs[0]],datestr)
+    data2 = get_info_for_day_from_server(my_users[pairs[1]],datestr)
+
+    if len(data1) == 0 or len(data2) == 0:
+        return None
+        
+    t0_1 = data1[0]['timestamp']
+    t0_2 = data2[0]['timestamp']
+    
+    t0 = t0_2
+    if t0_1 < t0_2:
+        t0 = t0_1
+        
+    tf_1 = data1[-1]['timestamp'] 
+    tf_2 = data2[-1]['timestamp']
+    
+    tf = tf_2
+    if tf_1 > tf_2:
+        tf = tf_1
+        
+        
+    targetkeys = ['value','on_duration_seconds', 'kickoff_counts']
+    N = (tf - t0) / 1000 / 60 + 1
+    t1 = [(item['timestamp'] - t0) / 1000 / 60 for item in data1]
+    t2 = [(item['timestamp'] - t0) / 1000 / 60 for item in data2]
+    m1 = [[item[targetkeys[0]], item[targetkeys[1]], item[targetkeys[2]]]  for item in data1]
+    m2 = [[item[targetkeys[0]], item[targetkeys[1]], item[targetkeys[2]]]  for item in data2]
+
+    pm1 = [[0, 0, 0] for i in xrange(N)]
+    pm2 = [[0, 0, 0] for i in xrange(N)]
+
+    for i in xrange(len(t1)):
+        pm1[t1[i]] = m1[i]
+        
+        
+    for i in xrange(len(t2)):
+        pm2[t2[i]] = m2[i]
+        
+    pm = np.concatenate( (np.array(pm1), np.array(pm2)),  axis=1)
+    
+    pca = MyPca()
+    pca.fit(pm, 2)
+    x2 = pca.transform(pm)
+    
+    plot(x2[:, 0],x2[:, 1],  '.')
+    show()
+    
+    
+    print len(data1), len(data2)
+
+    
+if __name__ == '__main__':
+    np.set_printoptions(precision=3, suppress=True, threshold=np.nan)
+
+    for i in range(len(user_partners)):
+        get_pair_data(user_partners[i], sys.argv[1])
