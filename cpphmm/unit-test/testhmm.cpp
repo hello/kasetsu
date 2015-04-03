3f819ea161958151b9418c2d9ebb3bd812e631b3
Benjamin Joseph <benjo@sayhello.com>
2015-04-03 16:42:40 -0700

    c++ HMM implementation, shell of a shell

---
+++ cpphmm/unit-test/testhmm.cpp
@@ -0,0 +1,22 @@
+#include "gtest/gtest.h"
+
+class TestHmm : public ::testing::Test {
+protected:
+    virtual void SetUp() {
+        
+    }
+    
+    virtual void TearDown() {
+        
+    }
+    
+    
+};
+
+
+TEST_F(TestHmm,TestFFT1) {
+    
+    
+
+}
+
