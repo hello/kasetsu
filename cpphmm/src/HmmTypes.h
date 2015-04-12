3abc0ed70a6f26e61c1804aac876303eeb070b6a
Benjamin Joseph <benjo@sayhello.com>
2015-04-11 23:56:27 -0700

    almost initialized model.

--- cpphmm/src/HmmTypes.h
+++ cpphmm/src/HmmTypes.h
@@ -9,4 +9,19 @@ typedef std::vector<HmmFloat_t> HmmDataVec_t;
 typedef std::vector<HmmDataVec_t> HmmDataMatrix_t;
 typedef std::vector<HmmDataMatrix_t> Hmm3DMatrix_t;
 
+/* for populating a vector  */
+template <class T>
+T & operator << (T & ref,typename T::value_type val) {
+    ref.push_back(val);
+    return ref;
+}
+
+template <class T>
+T & operator , (T & ref,typename T::value_type val) {
+    ref.push_back(val);
+    return ref;
+}
+
+
+
 #endif //_HMMTYPES_H_
