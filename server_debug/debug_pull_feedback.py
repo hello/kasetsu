65fcc6dae8c21f1521416a3991a493a395f35716
Benjamin Joseph <benjo@sayhello.com>
2015-04-29 15:43:21 -0700

    added debug_pull_feedback.py

---
+++ server_debug/debug_pull_feedback.py
@@ -0,0 +1,79 @@
+#!/usr/bin/python
+import datetime
+import calendar
+import requests
+import copy
+import csv
+
+
+#k_url = 'http://localhost:9997/v1/datascience/matchedfeedback/'
+k_url = 'https://research-api-benjo.hello.is/v1/datascience/matchedfeedback/'
+
+k_magic_auth = '7.e0aa1ca0289449f5b3b3c257da9523ec'
+#k_magic_auth = '2.26d34270933b4d5e88e513b0805a0644'
+
+k_headers = {'Authorization' : 'Bearer %s' % k_magic_auth}
+
+
+def get_datestr_as_timestamp(datestr):
+    mydate = datetime.datetime.strptime(datestr, '%Y-%m-%d')
+    return calendar.timegm(mydate.utctimetuple())*1000
+
+def get_time_as_string(timestamp,offset):
+    t = datetime.datetime.utcfromtimestamp(( offset + timestamp)/1000)
+    return t.strftime('%Y-%m-%d %H:%M:%S')
+
+def get_time_as_date(timestamp,offset):
+    t = datetime.datetime.utcfromtimestamp(( offset + timestamp)/1000)
+    return t.strftime('%Y-%m-%d')
+
+num_days = 7
+start_date_string = '2015-04-21'
+
+
+k_params = {'account_id' : 1012, 
+            'from_ts_utc' : get_datestr_as_timestamp(start_date_string), 
+            'num_days' : num_days, 
+}
+
+def pull_data():
+    responses = []
+    
+    params = copy.deepcopy(k_params)
+    headers = {'Authorization' : 'Bearer %s' % k_magic_auth}
+    
+    response = requests.get(k_url,params=params,headers=headers)
+        
+    if not response.ok:
+        print 'fail with %d on %s ' % (response.status_code,'foo')
+        return []
+
+    data = response.json()
+        
+    return data
+       
+def process_data(data):
+    for event in data:
+        event['delta'] = event['delta'] / 60000
+        event['date'] = get_time_as_date(event['date'], 0)
+        
+def data_to_csv(data, filename):
+    numrows = 0
+    with open(filename, 'w') as csvfile:
+        fieldnames = ['date','event_type','account_id', 'algorithm', 'delta', 'version']
+        
+        writer = csv.DictWriter(csvfile, fieldnames=fieldnames)
+
+        writer.writeheader()
+        
+        for event in data:
+            writer.writerow(event)
+            numrows += 1
+            
+    print "wrote %d rows" % numrows
+
+if __name__ == '__main__':
+    data = pull_data()
+    process_data(data)
+    data_to_csv(data, 'output.csv')
+
