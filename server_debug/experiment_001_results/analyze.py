b512baf6f73ce6c61eb57cf6cf8e7492de0d6b6e
Benjamin Joseph <benjo@sayhello.com>
2015-04-29 18:15:00 -0700

    now reporting number of normalized fails

--- server_debug/experiment_001_results/analyze.py
+++ server_debug/experiment_001_results/analyze.py
@@ -10,17 +10,38 @@ min_num_samples = 10
 success_threshold = 20 #minutes
 realbad_threshold = 60 #minutes
 
-def get_stats(x):
+
+keys_of_interest = ['IN_BED',  'SLEEP', 'WAKE_UP', 'OUT_OF_BED']
+alg_fractions = {'hmm' : 0.3, 'wupang' : 0.7}
+
+def get_fraction_from_key(key):
+    for alg in alg_fractions:
+        if alg in key:
+            return alg_fractions[alg]
+            
+    return None
+
+def get_stats(x, key):
+    
+    frac = get_fraction_from_key(key)
+
+    if frac == None:
+        return None
+        
     x = array(x)
     mydict = {}
+    
+    
     
     N = float(x.shape[0])
     mx = mean(x) 
     sx = std(x)
     medx = median(x)
     numsuccess = x[where(abs(x) <= success_threshold)].shape[0]
+    numfail = x[where(abs(x) > success_threshold)].shape[0]
     numrealbad = x[where(abs(x) <= realbad_threshold)].shape[0]
     psuccess = numsuccess / N
+    pfail = 1.0 - psuccess;
     prealbad = numrealbad / N
     
     denom_success = psuccess * (1 - psuccess)
@@ -35,19 +56,23 @@ def get_stats(x):
     mydict['num_samples'] = N
     mydict['success_ratio'] = psuccess
     mydict['realbad_ratio'] = prealbad
+    mydict['num_fail'] = numfail
     mydict['success_confidence'] = 2 / sqrt(fisher_information_success) #2 sigma
     mydict['realbad_confidence'] = 2 / sqrt(fisher_information_realbad) #2 sigma
+    mydict['num_normalized_fail'] = int(numfail / frac)
+    mydict['frac'] = frac
+    
 
 
     return mydict
 
 
 def dict_entry_to_list(key, entry):
-    return [key, entry['median'], entry['num_samples'], entry['success_ratio'], entry['success_confidence'], entry['realbad_ratio'], entry['realbad_confidence']]
+    return [key, entry['frac'], entry['num_normalized_fail'], entry['median'], entry['num_samples'], entry['success_ratio'], entry['success_confidence'], entry['realbad_ratio'], entry['realbad_confidence']]
 
 distributions = {}
 
-keys_of_interest = ['IN_BED',  'SLEEP', 'WAKE_UP', 'OUT_OF_BED']
+
 
 with open(filename, 'rb') as csvfile:
     reader = csv.DictReader(csvfile)
@@ -64,7 +89,10 @@ with open(filename, 'rb') as csvfile:
 results = {}
 for key in distributions.keys():
     try:
-        stats = get_stats(distributions[key])
+        stats = get_stats(distributions[key], key)
+        
+        if stats == None:
+            continue
         
         if stats['num_samples'] < min_num_samples:
             continue
@@ -77,7 +105,7 @@ keys = results.keys()
 keys.sort()
 
 mylist = []
-headers = ['alg and type','median error','# samp','success fraction','confidence','utter fail fraction','confidence']
+headers = ['alg and type','fraction of test','num normalized fails','median error','# samp','success fraction','confidence','utter fail fraction','confidence']
 for key in keys:
     mylist.append(dict_entry_to_list(key, results[key]))
 
