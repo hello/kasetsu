2229cb34fe224c02e357afbd0915b7bd984c1752
Benjamin Joseph <benjo@sayhello.com>
2015-05-18 13:26:22 -0700

    updated experiments1

--- server_debug/experiment_001_results/analyze.py
+++ server_debug/experiment_001_results/analyze.py
@@ -6,15 +6,18 @@ from numpy import *
 from tabulate import tabulate
 
 filename = 'experiment_001_results_raw.csv'
+logfile = 'logs.csv'
 min_num_samples = 10
 success_threshold = 20 #minutes
 realbad_threshold = 60 #minutes
 
+normalize_by_frequency_of_user = True
+
 alg_name_map = {'hmm' : 'HMM', 'wupang' : 'REGULAR'}
 
 keys_of_interest = ['IN_BED',  'SLEEP', 'WAKE_UP', 'OUT_OF_BED']
-alg_fractions = {'HMM' : 0.3, 'REGULAR' : 0.7}
-
+alg_fractions = {'HMM' : 0.203, 'REGULAR' : 0.797}
+    
 
 def get_fraction_from_key(key):
     for alg in alg_fractions:
@@ -23,18 +26,39 @@ def get_fraction_from_key(key):
             
     return None
 
-def get_stats(x, key):
+def get_stats(key, data, counts_by_account):
     
     frac = get_fraction_from_key(key)
-
+    
     if frac == None:
         return None
         
+    rows = data[key]
+
+
+    #get fail count, normalized by account id
+    count = 0
+    normalized_fail_count = 0.0
+    x = []
+    for row in rows:
+        delta = float(row['delta'])
+        account_id = row['account_id']
+        x.append(delta)
+        
+        cts = counts_by_account[account_id]
+        
+        normalizer = 1.0;
+        if normalize_by_frequency_of_user:
+            normalizer = 1.0 / cts
+        
+        if (abs(delta) > success_threshold):
+            normalized_fail_count += normalizer * 1.0 / frac
+        
     x = array(x)
+    
     mydict = {}
     
-    
-    
+
     N = float(x.shape[0])
     mx = mean(x) 
     sx = std(x)
@@ -45,7 +69,7 @@ def get_stats(x, key):
     psuccess = numsuccess / N
     pfail = 1.0 - psuccess;
     prealbad = numrealbad / N
-    
+        
     denom_success = psuccess * (1 - psuccess)
     denom_realbad = prealbad * (1 - prealbad)
     
@@ -61,43 +85,82 @@ def get_stats(x, key):
     mydict['num_fail'] = numfail
     mydict['success_confidence'] = 2 / sqrt(fisher_information_success) #2 sigma
     mydict['realbad_confidence'] = 2 / sqrt(fisher_information_realbad) #2 sigma
-    mydict['num_normalized_fail'] = int(numfail / frac)
+    mydict['num_normalized_fail'] = normalized_fail_count
     mydict['frac'] = frac
     
-
-
     return mydict
 
 
 def dict_entry_to_list(key, entry):
-    return [key, entry['frac'], entry['num_normalized_fail'], entry['median'], entry['num_samples'], entry['success_ratio'], entry['success_confidence'], entry['realbad_ratio'], entry['realbad_confidence']]
+    return [key, entry['frac'], entry['num_fail'],  entry['median'], entry['num_samples']]
 
-distributions = {}
-
-
-
-with open(filename, 'rb') as csvfile:
-    reader = csv.DictReader(csvfile)
-    
-    for row in reader:
-        et = row['event_type']
-        alg = row['algorithm']
+def get_valid_accounts():
+    accounts = []
+    with open(logfile, 'rb') as csvfile:
+        reader = csv.DictReader(csvfile)
         
-        if alg not in alg_name_map.keys():
-            continue
-        else:
-            alg = alg_name_map[alg]
-        
-        key = alg + '_' + et
-        if (not distributions.has_key(key)):
-            distributions[key] = []
+        for row in reader:
+            acc = row['account_id']
             
-        distributions[key].append(-int(row['delta']))
+            accounts.append(int(acc))
+            
+    return accounts
+
+if __name__ == '__main__':
+    counts_by_account = {}
+    data_by_alg = {}
+    
+    accounts = get_valid_accounts()
+    bad_accounts = {}
+    with open(filename, 'rb') as csvfile:
+        reader = csv.DictReader(csvfile)
         
-results = {}
-for key in distributions.keys():
-    try:
-        stats = get_stats(distributions[key], key)
+        for row in reader:
+            et = row['event_type']
+            alg = row['algorithm']
+            user = row['account_id']
+            
+            #if int(user) not in accounts:
+            #    bad_accounts[user] = 1
+            #    continue
+            
+            if alg not in alg_name_map.keys():
+                continue
+            else:
+                alg = alg_name_map[alg]
+                
+            if et not in keys_of_interest:
+                continue
+            
+            row['algorithm'] = alg
+            
+            key = alg + '_' + et
+            
+            
+            if (not data_by_alg.has_key(key)):
+                data_by_alg[key] = []
+                
+            if (not counts_by_account.has_key(user)):
+                counts_by_account[user] = 0
+                
+            data_by_alg[key].append(row)
+            counts_by_account[user] += 1
+    
+    #get sorted list of accounts
+    '''
+    sorted_account_ids_by_count = sorted(counts_by_account.items(), key=lambda x: x[1])
+    end_idx = int(len(sorted_account_ids_by_count) * 0.9)
+    sorted_account_ids_by_count = sorted_account_ids_by_count[0:end_idx]
+    
+    valid_accounts = [s[0] for s in sorted_account_ids_by_count]
+    print valid_accounts
+    '''
+        
+    print bad_accounts.keys()
+
+    results = {}
+    for key in data_by_alg.keys():
+        stats = get_stats(key, data_by_alg, counts_by_account)
         
         if stats == None:
             continue
@@ -106,20 +169,19 @@ for key in distributions.keys():
             continue
         
         results[key] = stats
-    except Exception:
-        foo = 3
-
-keys = results.keys()
-keys.sort()
-
-mylist = []
-headers = ['alg and type','fraction of test','num normalized fails','median error','# samp','success fraction','confidence','utter fail fraction','confidence']
-for key in keys:
-    mylist.append(dict_entry_to_list(key, results[key]))
-
-print ('\n\n\n\n\n\n\n')
-print ('success <= %d min error, fail > %d min, utter fail > %d min error, positive error = early prediction' % (success_threshold,success_threshold,  realbad_threshold))
-print('\n')
-print tabulate(mylist, headers=headers)
-print ('\n\n\n\n\n\n\n')
-
+        
+    
+    keys = results.keys()
+    keys.sort()
+    
+    mylist = []
+    headers = ['alg and type','fraction of test','num fails','median error', 'num feedbacks received']
+    for key in keys:
+        mylist.append(dict_entry_to_list(key, results[key]))
+    
+    print ('\n\n\n\n\n\n\n')
+    print ('fail > %d min, positive error = early prediction' % (success_threshold))
+    print('\n')
+    print tabulate(mylist, headers=headers)
+    print ('\n\n\n\n\n\n\n')
+    
