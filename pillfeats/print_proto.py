7d17c5e01dd7d4232931ed9c306bb0258cb94ce8
Benjamin Joseph <benjo@sayhello.com>
2015-06-01 09:42:21 -0700

    prints protobuf out

---
+++ pillfeats/print_proto.py
@@ -0,0 +1,52 @@
+#!/usr/bin/python
+
+import sleep_hmm_pb2
+import sys
+
+def print_state(state,statenum):
+    light = state.light
+    mot = state.motion_count
+    dist = state.disturbances
+    sound = state.log_sound_count
+    nat = state.natural_light_filter
+
+    printstr = '%02d: gam: %f,%f   psn: %f,  alph: %f,%f  gam: %f,%f, alph: %f,%f' % (statenum,light.mean,light.stddev,mot.mean,dist.probabilities[0],dist.probabilities[1],sound.mean,sound.stddev,nat.probabilities[0],nat.probabilities[1])
+    print printstr
+
+def get_vec_as_mat(A,N):
+    k = 0;
+    mat = []
+    for row in range(N):
+        mat.append([])
+        
+        for col in range(N):
+            mat[row].append(A[k])
+            k += 1
+
+    return mat
+
+if __name__ == '__main__':
+
+    f = open(sys.argv[1]);
+    s = f.read();
+    f.close()
+    q = sleep_hmm_pb2.SleepHmmModelSet()
+    q.ParseFromString(s.decode('base64'))
+
+    for model in q.models:
+        print "name=%s" % model.model_name
+
+        N = model.num_states
+        print "%d states" % N
+        
+        A = model.state_transition_matrix
+        mat = get_vec_as_mat(A,N)
+        for row in mat:
+            print row
+
+        statenum = 0
+        for state in model.states:
+            print_state(state,statenum)
+            statenum += 1
+
+        print '\n\n'
