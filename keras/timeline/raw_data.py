18b03cf0f1a0546b577f61bb50c2457d812149cc
Benjamin Joseph <benjo@sayhello.com>
2016-04-05 16:09:41 -0700

    half way there for labels

--- keras/timeline/raw_data.py
+++ keras/timeline/raw_data.py
@@ -5,6 +5,11 @@ import sys
 import datetime
 from math import *
 import copy
+import pytz
+import calendar
+
+def get_timestamp(dt):
+    return calendar.timegm(dt.utctimetuple())
 
 def read_24_hr_time(orig_time,dstr):
     t =  datetime.datetime.strptime(dstr,'%H:%M')
@@ -44,7 +49,8 @@ def read_input_file(filename):
         accs = []
         times = []
         for line in reader:
-            acc,t,x = sensor_data_vector_from_line(line)
+            acc,dt,x = sensor_data_vector_from_line(line)
+            t = get_timestamp(dt)
 
             if prev_account_id != acc:
                 if prev_account_id != None:
@@ -62,7 +68,7 @@ def read_input_file(filename):
 
     print '%d users' % len(all_sensor_data)
 
-    return accs,tt,all_sensor_data            
+    return accs,times,all_sensor_data            
 
 def read_label_file(filename):
     with open(filename,'rb') as f:
@@ -83,14 +89,32 @@ def read_label_file(filename):
             if not sleeps.has_key(acc):
                 sleeps[acc] = []
 
-            sleeps[acc].append(t1)
-            wakes[acc].append(t2)
+            sleeps[acc].append(get_timestamp(t1))
+            wakes[acc].append(get_timestamp(t2))
 
-    print sleeps
     return sleeps,wakes
+
+def create_labels_for_day(accounts,times,data,labels):
+    t1 = times[0][0];
+    t2 = times[0][-1];
+
+    for i,account in enumerate(accounts):
+        if not labels.has_key(account):
+            continue
+
+        xx = labels[account]
+        L = []
+        for x in xx:
+            if x >= t1 and x <= t2:
+                L.append(x)
+      
+        if len(L) > 0:
+            print L
+
+        
 
 
 if __name__ == '__main__':
-    read_label_file('labels_sleep_2016-01-01_2016-03-02.csv000')
-    read_input_file('2016-01-04.csv000')
-
+    sleeps,wakes=read_label_file('labels_sleep_2016-01-01_2016-03-02.csv000')
+    accounts,times,data=read_input_file('2016-01-04.csv000')
+    create_labels_for_day(accounts,times,data,wakes)   
