bc64f4b417383a452b46e3b210a90092fc29f883
Benjamin Joseph <benjo@sayhello.com>
2016-04-06 11:50:50 -0700

    possibly training

--- keras/timeline/train.py
+++ keras/timeline/train.py
@@ -1,43 +1,53 @@
+#!/usr/bin/python
 from keras.models import Sequential
 from keras.layers import LSTM, Dense
 import numpy as np
 import raw_data
-
+import sys
 def get_data():
     labels_file = 'labels_sleep_2016-01-01_2016-03-02.csv000'
     raw_data_files = ['2016-01-04.csv000']
 
-    data = raw_data.load_data(raw_data_files,labels_file)
+    return raw_data.load_data(raw_data_files,labels_file)
 
-    print len(data)
     
 
 def train():
-    data_dim = 16
-    timesteps = 8
-    nb_classes = 10
 
+    data = get_data();
+    d = data[0] #hope that this is a typical case
+    
+    data_dim = len(d[0][0])
+    timesteps = len(d[0])
+    nb_classes = len(d[1][0])
+
+    print "T=%d,N=%d,L=%d" % (timesteps,data_dim,nb_classes)
+
+    #concatenate all the classes
+    xx = []
+    ll = []
+    for x,l in data:
+        xx.append(x)
+        ll.append(l)
+
+    xx = np.array(xx)
+    ll = np.array(ll)
+
+        
     # expected input data shape: (batch_size, timesteps, data_dim)
     model = Sequential()
     model.add(LSTM(32, return_sequences=True,
                    input_shape=(timesteps, data_dim)))  # returns a sequence of vectors of dimension 32
     model.add(LSTM(32, return_sequences=True))  # returns a sequence of vectors of dimension 32
     model.add(LSTM(32))  # return a single vector of dimension 32
-    model.add(Dense(10, activation='softmax'))
+    model.add(Dense(nb_classes, activation='softmax'))
 
     model.compile(loss='categorical_crossentropy', optimizer='rmsprop')
 
-    # generate dummy training data
-    x_train = np.random.random((1000, timesteps, data_dim))
-    y_train = np.random.random((1000, nb_classes))
-
-    # generate dummy validation data
-    x_val = np.random.random((100, timesteps, data_dim))
-    y_val = np.random.random((100, nb_classes))
-
-    model.fit(x_train, y_train,
+ 
+    model.fit(xx, ll,
               batch_size=64, nb_epoch=5, show_accuracy=True,
-              validation_data=(x_val, y_val))
+              validation_data=(xx, ll))
 
 if __name__ == '__main__':
-    get_data()
+    train()
