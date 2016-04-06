4fbba30d4d623e069f396ce0ac8da5d44a16d5e5
Benjamin Joseph <benjo@sayhello.com>
2016-04-06 12:37:56 -0700

    training successfully, sorta

--- keras/timeline/train.py
+++ keras/timeline/train.py
@@ -1,6 +1,8 @@
 #!/usr/bin/python
 from keras.models import Sequential
-from keras.layers import LSTM, Dense
+from keras.layers import LSTM
+from keras.layers.core import TimeDistributedDense, Activation, Dropout
+from keras.optimizers import SGD
 import numpy as np
 import raw_data
 import sys
@@ -14,39 +16,57 @@ def get_data():
 
 def train():
 
+    print 'getting data...'
     data = get_data();
-    d = data[0] #hope that this is a typical case
-    
+
+    dt = []
+    for d in data:
+        dt.append(len(d[0]))
+
+    timesteps = np.median(dt)
     data_dim = len(d[0][0])
-    timesteps = len(d[0])
     nb_classes = len(d[1][0])
 
+    data2 = []
+    for d in data:
+        if len(d[0]) == timesteps:
+            data2.append(d)
+
+    print "found %d user-days" % len(data2)
     print "T=%d,N=%d,L=%d" % (timesteps,data_dim,nb_classes)
 
     #concatenate all the classes
     xx = []
     ll = []
-    for x,l in data:
+    for x,l in data2:
         xx.append(x)
         ll.append(l)
 
     xx = np.array(xx)
     ll = np.array(ll)
 
+    print xx.shape
+    print ll.shape
         
     # expected input data shape: (batch_size, timesteps, data_dim)
     model = Sequential()
-    model.add(LSTM(32, return_sequences=True,
-                   input_shape=(timesteps, data_dim)))  # returns a sequence of vectors of dimension 32
-    model.add(LSTM(32, return_sequences=True))  # returns a sequence of vectors of dimension 32
-    model.add(LSTM(32))  # return a single vector of dimension 32
-    model.add(Dense(nb_classes, activation='softmax'))
+    model.add(LSTM(32,return_sequences=True,input_shape=(int(timesteps),int(data_dim))))
+    model.add(Dropout(0.2))
+    model.add(LSTM(32,return_sequences=True))
+    model.add(Dropout(0.2))
+    model.add(LSTM(32,return_sequences=True))
+    model.add(Dropout(0.2))
+    model.add(TimeDistributedDense(nb_classes))
+    model.add(Activation('softmax'))
+    
+    print 'compiling...'
+    sgd = SGD(lr=0.1, decay=1e-6, momentum=0.9, nesterov=True)
+    model.compile(loss='categorical_crossentropy', optimizer=sgd)
 
-    model.compile(loss='categorical_crossentropy', optimizer='rmsprop')
 
- 
+    print 'fitting....'
     model.fit(xx, ll,
-              batch_size=64, nb_epoch=5, show_accuracy=True,
+              batch_size=16, nb_epoch=30, show_accuracy=True,
               validation_data=(xx, ll))
 
 if __name__ == '__main__':
