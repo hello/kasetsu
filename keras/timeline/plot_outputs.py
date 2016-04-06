b8bcb3cde0a6a6e2e359fcc5f4f51cdba3868f44
Benjamin Joseph <benjo@sayhello.com>
2016-04-06 15:54:34 -0700

    output plotting, trainer skips missing labels

---
+++ keras/timeline/plot_outputs.py
@@ -0,0 +1,67 @@
+#!/usr/bin/python
+from matplotlib.pyplot import *
+from keras.models import model_from_json
+import sys
+import raw_data
+
+def get_data():
+    labels_file = 'labels_sleep_2016-01-01_2016-03-02.csv000'
+    raw_data_files = ['2016-01-04.csv000']
+
+    return raw_data.load_data(raw_data_files,labels_file)
+
+def dostuff():
+    configdata = None
+    with open('my_config.json','r') as f:
+        configdata= f.read()
+    if configdata == None:
+        print 'could not read config file'
+        sys.exit(0)
+        
+    model = model_from_json(configdata)
+
+    model.load_weights('my_weights.h5')
+
+    print 'getting data...'
+    data = get_data();
+
+    dt = []
+    for d in data:
+        dt.append(len(d[0]))
+
+    timesteps = np.median(dt)
+    data_dim = len(d[0][0])
+    nb_classes = len(d[1][0])
+
+    data2 = []
+    for d in data:
+        if len(d[0]) == timesteps:
+            data2.append(d)
+
+    #concatenate all the classes
+    xx = []
+    ll = []
+    for x,l in data2:
+        xx.append(x)
+        ll.append(l)
+
+    xx = np.array(xx)
+    ll = np.array(ll)
+
+    print xx.shape
+    print ll.shape
+
+    print "found %d user-days" % len(data2)
+    print "T=%d,N=%d,L=%d" % (timesteps,data_dim,nb_classes)
+
+    p = model.predict(xx)
+
+    for i in range(p.shape[0]):
+        y = p[i]
+        x = xx[i]
+        plot(y); show()
+        
+
+if __name__ == '__main__':
+    dostuff()
+    
--- keras/timeline/train.py
+++ keras/timeline/train.py
@@ -3,12 +3,13 @@ from keras.models import Sequential
 from keras.layers import LSTM
 from keras.layers.core import TimeDistributedDense, Activation, Dropout
 from keras.optimizers import SGD
+from keras.optimizers import Adam
 import numpy as np
 import raw_data
 import sys
 def get_data():
     labels_file = 'labels_sleep_2016-01-01_2016-03-02.csv000'
-    raw_data_files = ['2016-01-04.csv000']
+    raw_data_files = ['2016-01-01.csv000','2016-01-02.csv000','2016-01-03.csv000','2016-01-04.csv000','2016-01-05.csv000','2016-01-06.csv000','2016-01-07.csv000']
 
     return raw_data.load_data(raw_data_files,labels_file)
 
@@ -32,6 +33,12 @@ def train():
         if len(d[0]) == timesteps:
             data2.append(d)
 
+    data = data2
+    data2 = []
+    for d in data:
+        if len(d[1]) > 0:
+            data2.append(d)
+    
     print "found %d user-days" % len(data2)
     print "T=%d,N=%d,L=%d" % (timesteps,data_dim,nb_classes)
 
@@ -60,14 +67,19 @@ def train():
     model.add(Activation('softmax'))
     
     print 'compiling...'
+    adam = Adam(lr=0.001, beta_1=0.9, beta_2=0.999, epsilon=1e-08)
     sgd = SGD(lr=0.1, decay=1e-6, momentum=0.9, nesterov=True)
-    model.compile(loss='categorical_crossentropy', optimizer=sgd)
+    model.compile(loss='categorical_crossentropy', optimizer=adam)
 
 
     print 'fitting....'
     model.fit(xx, ll,
-              batch_size=16, nb_epoch=30, show_accuracy=True,
+              batch_size=64, nb_epoch=25, show_accuracy=True,
               validation_data=(xx, ll))
+
+    with open('my_config.json','w') as f:
+        f.write(model.to_json())
+    model.save_weights('my_weights.h5')
 
 if __name__ == '__main__':
     train()
