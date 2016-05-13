a51c727033d55cf797879318a3ace7122c7f4a54
Benjamin Joseph <benjo@sayhello.com>
2016-05-13 16:14:03 -0700

    update to watchbot script

--- bots/device_id_watchdog/watch_for_devices.py
+++ bots/device_id_watchdog/watch_for_devices.py
@@ -6,7 +6,24 @@ import datetime
 import time
 import requests
 import logging as LOGGER
-k_devices = ['1005F47CFB40C4E9','04837B2F8DAD9D71','75DC4844E684EF07','5EE8ADB08573A954','4B42408AE6898D43','184D09D04FA9CCAA']
+
+k_devices = [
+'1005F47CFB40C4E9',
+'04837B2F8DAD9D71',
+'75DC4844E684EF07',
+'5EE8ADB08573A954',
+'4B42408AE6898D43',
+'184D09D04FA9CCAA',
+'03185E594B273B3C',
+'D8EF10B09CCDCD18',
+'F931EB0F5752FE5A',
+'E9D2498F2258FBEA',
+'C8DAAC353AEFA4A9',
+'E0A14B6AED2DA0FF',
+'87AAADAD23D98ADE',
+'AB9CB428A7837C86',
+'D78BD9241BAE0E56']
+
 k_slack_url = 'https://hooks.slack.com/services/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'
 k_db_url = 'common-replica-1.cdawj8qazvva.us-east-1.rds.amazonaws.com'
 k_db_username = 'common'
@@ -39,14 +56,16 @@ def get_query(querystr,devices):
 def post_new_accounts(accounts):
     r = requests.post(k_slack_url, json={'text': '@benjo new accounts found\n%s' % ('\n'.join(accounts)) })
 
-def post_startup_message(devices):
-    r = requests.post(k_slack_url, json={'text': 'SCRIPT STARTUP\n Looking for these guys: \n %s' % ('\n'.join(devices))})
+def post_startup_message(found_devices,devices):
+    r = requests.post(k_slack_url, json={'text': 'SCRIPT STARTUP\n Looking for these devices: \n%s \n\nAlready have these devices:\n%s' % ('\n'.join(devices),'\n'.join(found_devices))})
 
 if __name__=='__main__':
-    LOGGING.basicConfig(level=logging.DEBUG)
+    LOGGER.basicConfig(level=LOGGER.INFO)
     existing_accs = set()
     acc_device_map = {}
-    post_startup_message(k_devices)
+
+    is_first = True
+
     while(True):
         res = do_query()
 
@@ -59,10 +78,12 @@ if __name__=='__main__':
       
         new_acc_pairs = ['account=%s, device=%s' % (a,acc_device_map[a]) for a in new_accs]
 
-        if len(new_accs) > 0:
+        if is_first:
+            post_startup_message(new_acc_pairs,k_devices)
+            is_first = False
+        elif len(new_accs) > 0:
             post_new_accounts(new_acc_pairs)
 
-        
         time.sleep(60)
 
 
