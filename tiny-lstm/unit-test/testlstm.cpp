2f19b55ec1deff1abcdf37461b306961056cef1a
Benjamin Joseph <benjo@sayhello.com>
2016-05-25 17:24:40 -0700

    added unit test shells for all the layers

---
+++ tiny-lstm/unit-test/testlstm.cpp
@@ -0,0 +1,38 @@
+#include "gtest/gtest.h"
+#include "../tinylstm_types.h"
+#include "../tinylstm_math.h"
+#include "../tinylstm_conv_layer.h"
+#include "../tinylstm_tensor.h"
+
+
+class TestLstm : public ::testing::Test {
+protected:
+    
+    
+    virtual void SetUp() {
+        tensor_in = NULL;
+        tensor_out = NULL;
+    }
+    
+    virtual void TearDown() {
+        if (tensor_in) {
+            tensor_in->delete_me(tensor_in);
+        }
+        
+        if (tensor_out) {
+            tensor_out->delete_me(tensor_out);
+        }
+    }
+    
+    Tensor_t * tensor_in;
+    Tensor_t * tensor_out;
+    
+};
+
+class DISABLED_Test1 : public TestLstm {};
+
+
+TEST_F(TestLstm, TestFOO) {
+  //  tinylstm_convolve3d_direct(
+}
+
