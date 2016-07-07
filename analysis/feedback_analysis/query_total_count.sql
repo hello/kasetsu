96a31479501139f6e205b69c286e95177a8733f7
Benjamin Joseph <benjo@sayhello.com>
2016-07-07 11:01:46 -0700

    query total uses tracker motion

--- analysis/feedback_analysis/query_total_count.sql
+++ analysis/feedback_analysis/query_total_count.sql
@@ -1,3 +1,3 @@
 --run this on redshit
---SELECT date_trunc('day', ts) AS dd, COUNT(DISTINCT account_id) FROM tracker_motion_master WHERE ts > '2015-03-01' GROUP BY dd ORDER BY dd ASC;
-SELECT date_trunc('day', ts) AS dd, COUNT(DISTINCT account_id) FROM device_sensors_master WHERE ts > '2015-03-01' GROUP BY dd ORDER BY dd ASC;
+SELECT date_trunc('day', ts) AS dd, COUNT(DISTINCT account_id) FROM tracker_motion_master WHERE ts > '2015-03-01' GROUP BY dd ORDER BY dd ASC;
+--SELECT date_trunc('day', ts) AS dd, COUNT(DISTINCT account_id) FROM device_sensors_master WHERE ts > '2015-03-01' GROUP BY dd ORDER BY dd ASC;
