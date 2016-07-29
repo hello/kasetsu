b3953327f9d6f3f51bcad7340bfaab1bf4c52c7c
Benjamin Joseph <benjo@sayhello.com>
2016-07-29 10:38:31 -0700

    updated script to compute room response, not just fft

--- audio/lfsr/conv_ref.py
+++ audio/lfsr/conv_ref.py
@@ -4,6 +4,8 @@ from scipy.io import wavfile
 import numpy as np
 from matplotlib.pyplot import *
 
+PLOT_IMPULSE = True
+target_rate = 16000
 
 def load_data(filename):
     rate,data = wavfile.read(filename)
@@ -11,8 +13,8 @@ def load_data(filename):
     if len(data.shape) > 1:
         data = data[:,0]
 
-    if rate != 16000:
-        print 'file %s is not 16khz' % (filename)
+    if rate != target_rate:
+        print 'file %s is not %dhz' % (filename,target_rate)
         sys.exit(0)
 
     data = data / 32768.
@@ -22,7 +24,7 @@ def load_data(filename):
 def conv_data(data,ref):
 
     N = data.shape[0] / ref.shape[0]
-
+    print N
     if N < 3:
         print('data is not long enough')
         sys.exit(0)
@@ -35,12 +37,17 @@ def conv_data(data,ref):
     x2 = x[begin:end]
     i = np.argmax(np.abs(x2))
 
-    begin2 = i - 20
+    begin2 = i - 5
     end2 = begin2 + 1024
     xx = x2[begin2:end2]
 
+    if PLOT_IMPULSE:
+        dist = np.arange(xx.shape[0]).astype(float) / target_rate * 340.0 / 2.0
+        xx2 = xx / np.max(np.abs(xx))
+        plot(dist,xx2); xlabel('distance (m)'); title('normalized room response'); show()
+
     h = np.fft.fft(xx)
-    f = np.linspace(0,8000,len(h)/2)
+    f = np.linspace(0,target_rate,len(h)/2)
     a = 20*np.log10(np.abs(h[0:len(h)/2]))
     return f,a
 
