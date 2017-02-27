820c7ba874270a7c670aad7d5ef4bd803515d7d5
Benjamin Joseph <benjo@sayhello.com>
2017-02-27 08:07:35 -0800

    almost have features

--- audio/acoustic_model/ctc.py
+++ audio/acoustic_model/ctc.py
@@ -1,9 +1,10 @@
 from keras.preprocessing import sequence
 from keras.models import Model
-from keras.layers import Dense,TimeDistributedDense Activation, Embedding,Input,LSTM,Lambda
+from keras.layers import Dense, Activation, Embedding,Input,LSTM,Lambda
 from keras import backend as K
 from keras.optimizers import SGD
-
+import hello_audio_feats
+import sys
 
 '''
 ctc_batch_cost(y_true, y_pred, input_length, label_length)
@@ -33,36 +34,71 @@ def ctc_lambda_func(args):
     y_pred = y_pred[:, 2:, :]
     return K.ctc_batch_cost(labels, y_pred, input_length, label_length)
 
-num_phonemes = 46
+def data_to_vecs(data,label_sequences):
+    max_time = 0
+    max_symbols = 0
+    #find maximum length of labels and features so we can get the embeddings right
+    for key in data:
+        
+        q = data[key].shape[0]
+        
+        if q > max_time:
+            max_time = q
+
+        p = len(label_sequences[key])
+
+        if p > max_symbols:
+            max_symbols = p
 
 
-symbol_indices_map = get_all_unique_symbols()
-sequences_map = get_symbol_index_sequence(symbol_indices_map)
+    print max_time,max_symbols
+
+    
+
+    
+
+def main():
+    data,label_sequences,symbol_indices = hello_audio_feats.read_all_data('./data/')
+
+    data_to_vecs(data,label_sequences)
+    
+    num_phonemes = len(symbol_indices)
+
+    sys.exit(0)
+    
+    symbol_indices_map = get_all_unique_symbols()
+    sequences_map = get_symbol_index_sequence(symbol_indices_map)
 
 
-labels = Input(name='the_labels', shape=[img_gen.absolute_max_string_len], dtype='float32')
+    labels = Input(name='the_labels', shape=[img_gen.absolute_max_string_len], dtype='float32')
 
 
 
-#the neural net
-input_data = Input(name='the_input', shape=input_shape, dtype='float32')
-lstm1 = LSTM(32)(input_data,return_sequences=True,name='LSTM1')(input_data)
-lstm2 = LSTM(32,return_sequences=True,name='LSTM2')(lstm1)
-dense = Dense(num_phonemes,name='DENSE')(lstm2)
-y_pred = Activation('softmax', name='SOFTMAX')(dense)
+    #the neural net
+    input_data = Input(name='the_input', shape=input_shape, dtype='float32')
+    lstm1 = LSTM(32)(input_data,return_sequences=True,name='LSTM1')(input_data)
+    lstm2 = LSTM(32,return_sequences=True,name='LSTM2')(lstm1)
+    dense = Dense(num_phonemes,name='DENSE')(lstm2)
+    y_pred = Activation('softmax', name='SOFTMAX')(dense)
 
-#stuff for the loss function
-input_length = Input(name='input_length', shape=[1], dtype='int64')
-label_length = Input(name='label_length', shape=[1], dtype='int64')
-labels = Input(name='LABELS', shape=[num_phonemes], dtype='float32')
-loss_out = Lambda(ctc_lambda_func, output_shape=(1,), name='ctc')([y_pred, labels, input_length, label_length])
+    #stuff for the loss function
+    input_length = Input(name='input_length', shape=[1], dtype='int64')
+    label_length = Input(name='label_length', shape=[1], dtype='int64')
+    labels = Input(name='LABELS', shape=[num_phonemes], dtype='float32')
+    loss_out = Lambda(ctc_lambda_func, output_shape=(1,), name='ctc')([y_pred, labels, input_length, label_length])
 
-#the model with the net and the loss function
-model = Model(input=[input_data, labels, input_length, label_length], output=[loss_out])
+    #the model with the net and the loss function
+    model = Model(input=[input_data, labels, input_length, label_length], output=[loss_out])
 
-#optimizer
-sgd = SGD(lr=0.02, decay=1e-6, momentum=0.9, nesterov=True, clipnorm=5)
+    #optimizer
+    sgd = SGD(lr=0.02, decay=1e-6, momentum=0.9, nesterov=True, clipnorm=5)
 
-#compile!
-model.compile(loss={'ctc': lambda y_true, y_pred: y_pred}, optimizer=sgd)
+    #compile!
+    model.compile(loss={'ctc': lambda y_true, y_pred: y_pred}, optimizer=sgd)
+
+    #fit
+    #model.fit(self, x, y, batch_size=32, nb_epoch=10)
+
+if __name__ == '__main__':
+    main()
 
---
+++ audio/acoustic_model/hello_audio_feats.py
@@ -0,0 +1,93 @@
+
+import glob
+import csv
+import os
+import struct
+import numpy as np
+import sys
+
+filetypestr = '*.lab'
+data_file_ending = '.wav.bin'
+
+def int16_unpack_array(bindata):
+    N = len(bindata) / 2
+    x = []
+    for i in range(N):
+        d = bindata[2*i:2*(i+1)]
+        val = struct.unpack('h',d)
+        x.append(val)
+
+    return np.array(x)
+
+def get_feats_file(fname):
+    print fname
+    f = open(fname,'r')
+    x = int16_unpack_array(f.read())
+    x = x.reshape(x.shape[0] / 40,40).astype(float)
+    f.close()
+    return x
+
+
+def get_key_from_filename(fname):
+    fname = os.path.basename(fname)
+    return fname.split('.')[0]
+
+def get_symbols_from_file(fname):
+
+    items = []
+    with open(fname,'r') as csvfile:
+        reader = csv.reader(csvfile,delimiter=' ')
+
+        for line in reader:
+            items.append(line[2])
+
+    return items
+
+
+def get_symbol_index_sequence(symbol_indices,relpath):
+    files = glob.glob(relpath + filetypestr)
+    sequences = {}
+    for fname in files:
+        symbols_sequence = get_symbols_from_file(fname)
+        sequences[get_key_from_filename(fname)] = [symbol_indices[s] for s in symbols_sequence]
+
+    return sequences
+        
+
+def get_all_unique_symbols(relpath):
+    files = glob.glob(relpath + filetypestr)
+
+    symbols = set([])
+    for fname in files:
+        symbols |=  set(get_symbols_from_file(fname))
+
+    symbols = sorted(symbols)
+
+    symbol_map = {}
+    for i in range(len(symbols)):
+        symbol_map[symbols[i]] = i
+        
+    return symbol_map
+
+
+def read_all_data(relpath):
+    symbol_indices = get_all_unique_symbols(relpath)
+    label_sequences = get_symbol_index_sequence(symbol_indices,relpath)
+
+    data = {}
+    for key in label_sequences:
+        x = get_feats_file(relpath + key + data_file_ending)
+        data[key] = x
+
+    return data,label_sequences,symbol_indices
+    
+
+    
+def main():
+    data,label_sequences,symbol_indices = read_all_data(sys.argv[1])
+    print label_sequences
+    print len(symbol_indices)
+    
+    
+if __name__ == '__main__':
+    main() 
