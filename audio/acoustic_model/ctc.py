defa647daccd7fc1d71803ebd9016605335614a7
Benjamin Joseph <benjo@sayhello.com>
2017-02-27 21:55:17 -0800

    working under tensorflow backend moar reliably

--- audio/acoustic_model/ctc.py
+++ audio/acoustic_model/ctc.py
@@ -1,8 +1,10 @@
 from keras.preprocessing import sequence
 from keras.models import Model
 from keras.layers import TimeDistributed,Dense, Activation, Embedding,Input,LSTM,Lambda
+import keras.callbacks
+
 from keras import backend as K
-from keras.optimizers import SGD
+from keras.optimizers import SGD,Adam
 import hello_audio_feats
 import sys
 import numpy as np
@@ -33,8 +35,13 @@ def ctc_lambda_func(args):
     y_pred, labels, input_length, label_length = args
     # the 2 is critical here since the first couple outputs of the RNN
     # tend to be garbage:
-    y_pred = y_pred[:, 2:, :]
+    #y_pred = y_pred[:, 2:, :]
     return K.ctc_batch_cost(labels, y_pred, input_length, label_length)
+
+def ctc_lambda_decode(args):
+    y_pred,input_length = args
+    return K.ctc_decode(y_pred, input_length, greedy=False, beam_width=10, top_paths=1)
+
 
 def data_to_vecs(data,label_sequences):
     max_time = 0
@@ -65,7 +72,7 @@ def data_to_vecs(data,label_sequences):
     for key in data:
         xd = data[key]
         x[count,0:xd.shape[0],0:xd.shape[1]] = xd
-        input_lengths.append(xd.shape[1])
+        input_lengths.append(xd.shape[0])
         
         ld = np.array(label_sequences[key],dtype='int64')
         labels[count,0:ld.shape[0]] = ld
@@ -87,23 +94,20 @@ def main():
     input_shape = (x.shape[1],x.shape[2])
     num_phonemes = len(symbol_indices)
 
-
-    
     #symbolic graph stuff#
-
     #the neural net
     input_data = Input(name='the_input', shape=input_shape, dtype='float32')
     lstm1 = LSTM(32,return_sequences=True,name='LSTM1')(input_data)
     lstm2 = LSTM(32,return_sequences=True,name='LSTM2')(lstm1)
-    dense = TimeDistributed(Dense(num_phonemes,name='DENSE'))(lstm2)
+    dense = TimeDistributed(Dense(num_phonemes + 1,name='DENSE'))(lstm2)
     y_pred = Activation('softmax', name='SOFTMAX')(dense)
 
     #stuff for the loss function
-    input_length = Input(name='input_length', shape=[1], dtype='int64')
-    label_length = Input(name='label_length', shape=[1], dtype='int64')
+    input_length = Input(name='input_length', shape=[1], dtype='int32')
+    label_length = Input(name='label_length', shape=[1], dtype='int32')
     labels = Input(name='LABELS', shape=[max_symbols], dtype='float32')
     loss_out = Lambda(ctc_lambda_func, output_shape=(1,), name='ctc')([y_pred, labels, input_length, label_length])
-
+#    predict_out = Lambda(ctc_lambda_decode,output_shape=(1,),name='decode')([y_pred,input_length])
     #the model with the net and the loss function
     model = Model(input=[input_data, labels, input_length, label_length], output=[loss_out])
 
@@ -113,8 +117,12 @@ def main():
     #compile!
     model.compile(loss={'ctc': lambda y_true, y_pred: y_pred}, optimizer=sgd)
 
+    filepath = 'test' + '_weights.{epoch:02d}-{val_loss:.2f}.h5'
+    checkpoint_callback = keras.callbacks.ModelCheckpoint('weights.{epoch:03d}.h5', verbose=0, save_best_only=False, save_weights_only=False, mode='auto', period=1)
+
     #fit
-    model.fit([x,x_labels,x_in_lens,x_label_lens], [x_labels], batch_size=32, nb_epoch=10)
+    model.fit([x,x_labels,x_in_lens,x_label_lens], [x_labels], batch_size=32, nb_epoch=2,callbacks = [checkpoint_callback])
+
 
 if __name__ == '__main__':
     main()
